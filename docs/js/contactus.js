9cc8b8fe7a369fd68e51a4db5391290e83e53456
Command <maoxs2@gmail.com>
2020-05-23 22:55:00 +0800

    regen & replace title with topic

--- docs/js/contactus.js
+++ docs/js/contactus.js
@@ -1,11 +1,11 @@
 
     document.getElementById("post_contact_us").onclick = function() {
-        var title = document.getElementById("contact_title").value;
+        var topic = document.getElementById("contact_topic").value;
         var name = document.getElementById("contact_name").value;
         var email = document.getElementById("contact_email").value;
         var body = document.getElementById("contact_message").value;
         if (title != undefined && body != undefined) {
-            window.open("https://github.com/ngchain/contactus/issues/new?title=" + title + "&body=From: " + name + " [" + email +  "]%0a%0a" + + body);
+            window.open("https://github.com/ngchain/contactus/issues/new?title=" + topic + "&body=From: " + name + " [" + email +  "]%0a%0a" + + body);
         }
     }
 
